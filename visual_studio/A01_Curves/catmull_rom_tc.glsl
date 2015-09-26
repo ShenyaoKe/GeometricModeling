f0d847d09e0c55fb80e3eaaa3efb3b1c5795171a
ShenyaoKe <akika@tamu.edu>
2015-09-26 10:01:48 -0500

    add separate tessellation control shader for catmull-rom spline

---
+++ visual_studio/A01_Curves/catmull_rom_tc.glsl
@@ -0,0 +1,14 @@
+#version 440
+layout(vertices = gl_MaxPatchVertices) out;
+
+uniform int segments;
+uniform int NumStrips = 1;
+
+void main()
+{
+	// Pass along the vertex position unmodified
+	gl_out[gl_InvocationID].gl_Position = gl_in[gl_InvocationID].gl_Position;
+	// Define the tessellation levels
+	gl_TessLevelOuter[0] = float(NumStrips);
+	gl_TessLevelOuter[1] = float(segments);
+}
