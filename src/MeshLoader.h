ff0d6c34df8140bd0ef5e3a840b647558ea742fb
ShenyaoKe <akika@tamu.edu>
2016-04-06 19:27:20 -0500

    fixed EOF error in load_from_file function.

--- src/MeshLoader.h
+++ src/MeshLoader.h
@@ -41,7 +41,7 @@ public:
 	};
 
 	void release();
-	void load(const char* filename);
+	void load_from_string(const char* filename);
 	void load_from_file(const char* filename);
 
 	void exportVBO(floats_t* verts = nullptr,
