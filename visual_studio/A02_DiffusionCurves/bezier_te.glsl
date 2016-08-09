e1ec15314b195244c7cee8efebab9c02b4b94e98
ShenyaoKe <akika@tamu.edu>
2016-08-08 22:01:05 -0700

    simplify curve derivative

--- visual_studio/A02_DiffusionCurves/bezier_te.glsl
+++ visual_studio/A02_DiffusionCurves/bezier_te.glsl
@@ -5,8 +5,14 @@ out vec2 uv_te;
 //out vec4 T_te;
 out vec4 B_te;
 
-vec3 BezierBlossom(float u)
+void main()
 {
+	////
+	// Bezier Curve
+    // The tessellation u coordinate
+	float u = gl_TessCoord.x;
+	float v = gl_TessCoord.y;
+
 	// The patch vertices (control points)
 	vec3 p[9];
 	for (int i = 0; i <= degree; i++)
@@ -21,24 +27,8 @@ vec3 BezierBlossom(float u)
 			p[i] = p[i] * (1.0 - u) + p[i + 1] * u;
 		}
 	}
-	return p[0];
-}
-
-void main()
-{
-	////
-	// Bezier Curve
-    // The tessellation u coordinate
-	float u = gl_TessCoord.x;
-	float v = gl_TessCoord.y;
-
-	// The patch vertices (control points)
-	vec3 curPos[3];
-	curPos[0] = BezierBlossom(u - 0.001);
-	curPos[1] = BezierBlossom(u);
-	curPos[2] = BezierBlossom(u + 0.001);
-	gl_Position = vec4(curPos[1], 1.0);// +vec4(0, v*0.1, 0, 0);
-	vec3 T_te = normalize(curPos[2] - curPos[0]);
+	gl_Position = vec4(p[0], 1.0);
+	vec3 T_te = normalize(p[1] - p[0]);
 	B_te = vec4(cross(vec3(0, 0, 1), T_te), 0.0);
 	uv_te = gl_TessCoord.xy;
 }
